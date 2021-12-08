318d7f76ec88d2c916df0bcb031b2eef954b77a6
Vahid <93490818+VahidFaraj07@users.noreply.github.com>
2021-12-09 00:12:00 +0400

    QLabel

--- simpleLabel.py
+++ simpleLabel.py
@@ -8,14 +8,13 @@ class Window(QWidget):
         self.setGeometry(200, 300, 500, 400)
         self.setWindowTitle('Label Example')
         self.UI()
+        self.show()
 
     def UI(self):
         self.text1 = QLabel('- hello bro', self)
         self.text2 = QLabel('- hi man', self)
         self.text1.move(10, 100)
         self.text2.move(10, 120)
-
-        self.show()
 
 
 def main():
