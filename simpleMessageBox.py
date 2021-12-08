5cdac3a6c8a8e8f1cc50c436faddc6897ab5aca9
Vahid <93490818+VahidFaraj07@users.noreply.github.com>
2021-12-09 00:13:36 +0400

    QMessageBox

--- simpleMessageBox.py
+++ simpleMessageBox.py
@@ -2,9 +2,9 @@ import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QFont
 
-
 myFont1 = QFont("Boardway")
 myFont2 = QFont("Candara")
+
 
 class Window(QWidget):
     def __init__(self):
@@ -13,7 +13,7 @@ class Window(QWidget):
         self.setWindowTitle('MessageBox Example')
         self.UI()
         self.show()
-    
+
     def UI(self):
         infoButton = QPushButton("Info", self)
         exitButton = QPushButton("Click me", self)
@@ -21,25 +21,27 @@ class Window(QWidget):
         infoButton.setFont(myFont1)
         exitButton.setFont(myFont2)
 
-        infoButton.move(50,50)
-        exitButton.move(150,50)
+        infoButton.move(50, 50)
+        exitButton.move(150, 50)
 
         infoButton.clicked.connect(self.myInfoMessage)
         exitButton.clicked.connect(self.myExitMessage)
 
     def myInfoMessage(self):
-        message = QMessageBox.information(self, "Info","Life has no Ctrl + Z")
+        message = QMessageBox.information(self, "Info", "Life has no Ctrl + Z")
+
     def myExitMessage(self):
-        message = QMessageBox.question(self, "Warning","Are you sure you want to exit?", QMessageBox.Yes | QMessageBox.No| QMessageBox.Cancel)
+        message = QMessageBox.question(
+            self, "Warning", "Are you sure you want to exit?", QMessageBox.Yes | QMessageBox.No | QMessageBox.Cancel)
         if message == QMessageBox.Yes:
             sys.exit()
-
 
 
 def main():
     App = QApplication(sys.argv)
     window = Window()
     sys.exit(App.exec_())
+
 
 if __name__ == '__main__':
     main()
